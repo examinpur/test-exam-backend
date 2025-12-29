6e5968fcd6bb7f0b4e07f0fbb8bad3c1e63d1215
coderoofsit <141501346+coderoofsit@users.noreply.github.com>
2025-12-29 12:35:39 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,16 @@ import paperRoutes from './modules/paper/routes/paperRoutes';
 dbConnection();
 const app = express();
 
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true,
+  origin: "*",
+  credentials: false,
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
